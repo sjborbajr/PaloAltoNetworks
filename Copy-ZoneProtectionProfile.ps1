d5737aa83b7ab351034cc52c95f2c35cd59959bd
Steve Borba <sjborbajr@users.noreply.github.com>
2024-04-03 09:39:44 -0700

    Create Copy-ZoneProtectionProfile.ps1

---
+++ Copy-ZoneProtectionProfile.ps1
@@ -0,0 +1,24 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+Import-Module pan-power
+
+
+$Source = 'Global'
+$Profile = 'default'
+
+
+
+$Source = "/config/devices/entry/template/entry[@name='$Source']/config/devices/entry/network/profiles/zone-protection-profile/entry[@name='$Profile']"
+$ZPP = (Get-PANConfig -XPath $Source).result.entry.OuterXml
+$Destinations = (Get-PANConfig -XPath "/config/devices/entry/template").result.template.entry.name | Out-GridView -Title "Select destination(s)" -OutputMode Multiple
+ForEach ($Destination in $Destinations){
+  $XPath = "/config/devices/entry/template/entry[@name='$Destination']/config/devices/entry/network/profiles/zone-protection-profile/entry[@name='$Profile']"
+  $Test = Get-PANConfig -XPath $XPath
+  If ($Test.result.count -eq 1) {
+    $Test = "copy"
+  } else {
+    #ask if we should create?
+  }
+  If ($Test -eq 'copy') {
+    $result = Edit-PANConfig -XPath $XPath -Data $ZPP
+  }
+}
