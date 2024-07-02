b1ef9797dfe88e08f58e8cfbdd20692b2d71e77a
Steve Borba <sjborbajr@users.noreply.github.com>
2024-07-02 16:26:49 -0700

    Create Backup-PANConfig.ps1

---
+++ Backup-PANConfig.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+Import-Module pan-power
+
+#Gather Data
+$Devices = (Invoke-PANOperation -SkipCertificateCheck -Command "<show><devices><connected></connected></devices></show>").result.devices.entry
+for ($ix = 0; $ix -lt $Devices.count; $ix++) {
+  $Result = Get-PANConfig -Target $Devices[$ix].serial -Show
+  if ($Result.status -eq 'success') {
+    $Result.result.config.OuterXml > ($Devices[$ix].hostname+'.xml')
+  } else {
+    write-host ("Unable to get config "+$Devices[$ix].hostname)
+  }
+}
